bb56f6cc01630b72c9c1268cabe8ef018b534a39
AnirudhVijayaraghavan <114264437+AnirudhVijayaraghavan@users.noreply.github.com>
2023-09-27 20:01:49 -0400

    Final commit.
    Fixed DB connectivity.
    Severed DB connection shows 503 now.

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ console.log("Starting program.")
 // const flag = require('./databasepg')
 const express = require('express');
 const app = express();
-const port = 3000; // You can change the port as needed
+const port = 3000; // Connects to localhost:3000
 
 //
 const { Pool } = require('pg');
 
 
 async function checkDatabaseConnection() {
+  // The object below has the postgres test DB credentials / configurations. Port : 5432, name : test, user : postgres.
   const dbConfig = {
     host: "localhost",
     user: "postgres",
@@ -17,40 +18,26 @@ async function checkDatabaseConnection() {
     database: "test"
   };
   const pool = new Pool(dbConfig);
-  // Replace these with your actual PostgreSQL database credentials
+
 
   try {
-    // Attempt to connect to the PostgreSQL database
+    // The line below connects to the main test postgres DB.
     const client = await pool.connect();
-    console.error('Checkpoint 1');
-    // If the connection is successful, release it and return true
+    // Releases the resource on successful connection.
     client.release();
     return true;
   } catch (error) {
-    console.error('Database connection error:', error);
+    //console.error('Database connection error:', error);
     return false;
   } finally {
     pool.end();
   }
 
-  // flag = false;
-  // client.query("select * from users", (err, res) => {
-  //   if (!err) {
-  //     console.log(res.rows);
-  //     flag = true;
-  //   } else {
-  //     console.log(err.message);
-  //     flag =  false;
-  //   }
-  // })
-  // return flag;
-  // client.end();
-
 }
 //
 
 
-// Middleware to add Cache-Control header
+// Adds the Cache-control on the Header.
 app.use((req, res, next) => {
   res.setHeader('Cache-Control', 'no-cache');
   next();
@@ -60,22 +47,25 @@ app.use((req, res, next) => {
 app.get('/healthz', (req, res) => {
   // Simulate a database connection check (replace with your own logic)
   const isDBConnected = checkDatabaseConnection();
-
-  if (isDBConnected) {
-    res.status(200).send();
-    console.log("200");
-  } else {
-    res.status(503).send();
-    console.log("503");
-  }
+  let flag = isDBConnected.then((result) => {
+    //console.log(result); // "Promise resolved"
+    if (result) {
+      res.status(200).send();
+      console.log("200");
+    } else {
+      res.status(503).send();
+      console.log("503");
+    }
+  })
 });
-// Handle unsupported HTTP methods for /healthz
+// The following code handles all other HTTP requests. Sends 405.
 app.all('/healthz', (req, res) => {
   res.status(405).send();
-  console.log("405")
+  console.log("405");
 });
 
-// Start the server
+
+// Starts the server, on port 3000.
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
